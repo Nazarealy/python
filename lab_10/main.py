3f2ad80e8c2491dfc3f985d084d24286f9ffb40a
Nazarii <wilfreaport@gmail.com>
2019-05-19 23:49:28 +0300

    gg

--- lab_10/main.py
+++
@@ -1,2 +0,0 @@
-from sqeezer import Sqeezer
-
--- lab_10/sqeezer.py
+++ lab_10/sqeezer.py
@@ -1,8 +1,40 @@
 class Sqeezer:
-    
+
+    staticProdaction = 100
+
+    def __init__(self, color = "%variant%", sqeezSpeed = 0, enginePower = 0, founder = "%variant%", price = 0, rating = 0.0, material = "%variant%"):
+        self.color = color
+        self.sqeezSpeed = sqeezSpeed
+        self.enginePower = enginePower
+        self.founder = founder
+        self.price = price
+        self.rating = rating
+        self.material = material
+
+    def __del__(self):
+        print("%s deleted" % self.color)
+
+    def __str__(self):
+        return "color: {0}, sqeezSpeed: {1}, enginePower: {2}founder: {3},\nprice: {4}, rating: {5}, material: {6}.\n".format(self.color, self.sqeezSpeed, 
+            self.enginePower, self.founder, self.price, self.rating, self.material)   
+
+    @staticmethod
+    def anotherstaticProdaction(newstaticProdaction):
+            Sqeezer.staticProdaction += newstaticProdaction
+
+defalt = Sqeezer()
+juicy = Sqeezer("red", 15, 220, "German", 15, 5, "plastick")
+
+print(defalt)
+print(juicy)
+
+print("Total number of prodaction: " + str(Sqeezer.staticProdaction))
+Sqeezer.anotherstaticProdaction(50)
+print("Total number of prodaction: " + str(Sqeezer.staticProdaction))
+print()
     
 
     
 
-    
-    
+
+   
